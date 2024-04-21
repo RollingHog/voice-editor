3f9f63e24de81223b7689e11f1ae086e2faa3a8d
RollingHog <46373241+RollingHog@users.noreply.github.com>
2024-04-21 23:02:36 +0300

    fix 2

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const APP_PREFIX = 'rh_voed_dsc'
 // The version of the cache. Every time you change any of the files
 // you need to change this version.
 // eslint-disable-next-line no-unused-vars
-const VERSION = '1.0.0'
+const VERSION = '1.0.1'
 
 // The files to make available for offline use
 // eslint-disable-next-line no-unused-vars
